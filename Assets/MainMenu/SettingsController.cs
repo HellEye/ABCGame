91803da7e6183567b53a37628a1b013afe952fb4
Gordon Piasecki <g.piasecki@cyfrowe.org>
2026-07-29 14:53:49 +0200

    added settings popup window frame with tabs and reset/close buttons

--- Assets/MainMenu/SettingsController.cs
+++ Assets/MainMenu/SettingsController.cs
@@ -8,7 +8,6 @@ public class SettingsController : MonoBehaviour {
     [SerializeField] GameObject spriteScalingReference;
     [Inject] UIDocument document;
     [Inject] MainMenuSettingsData settingsData;
-
     //string[] tabsNamesWithoutScaling;
 
     void Awake() {
@@ -16,7 +15,7 @@ public class SettingsController : MonoBehaviour {
         EnumDropdownBinding.RegisterConverter(MainMenuSettingsData.ColorblindTextGetter);
         var rootElement = document.rootVisualElement;
         //tabsNamesWithoutScaling[0] = "TabA";
-
+        var tabView = rootElement.Q<TabView>("settings-tabs");
         // settings popup
         var settingsPopup = rootElement.Q<Popup>("settings-popup");
         settingsPopup.dataSource = settingsData;
@@ -24,13 +23,12 @@ public class SettingsController : MonoBehaviour {
         settingsPopup.WithCloseButton(rootElement.Q<Button>("settings-close"));
 
         // settings button bindings
-        settingsPopup.Q<Button>("settings-save").clicked += () => settingsData.Save();
         rootElement.Q<Button>("options").clicked += () => {
             spriteScalingReference.SetActive(settingsPopup.Q<TabView>("settings-tabs").activeTab.name == "TabB");
         };
-        settingsPopup.Q<Button>("settings-close").clicked += () => {
+        settingsPopup.Q<Button>("settings-close").clicked += () =>
+        {
             spriteScalingReference.SetActive(false);
-            settingsData.Load();
         };
         settingsPopup.Q<TabView>("settings-tabs").activeTabChanged += (oldTab, newTab) => {
             if (newTab?.name == "TabB")
@@ -41,7 +39,11 @@ public class SettingsController : MonoBehaviour {
                 spriteScalingReference.SetActive(false);
         };
 
-        settingsPopup.Q<Button>("settings-reset").clicked += () => settingsData.Reset();
+        settingsPopup.Q<Button>("settings-reset").clicked += () =>
+        {
+            settingsData.Reset();
+            settingsData.Save();
+        };
 
         // dropdown bindings
         EnumDropdownBinding.SetChoices(
@@ -56,37 +58,19 @@ public class SettingsController : MonoBehaviour {
         EnumDropdownBinding.SetChoices(
             settingsPopup.Q<DropdownField>("colorblind-dropdown"),
             MainMenuSettingsData.ColorblindTextGetter);
-        SampleBinding();
+        SetupAutoSave();
     }
 
     [ContextMenu("Test Binding")]
     public void Test() => settingsData.VoVolume += 10;
 
-    void SampleBinding() =>
-        // Example of how to bind to settings data
-        settingsData.propertyChanged += (sender, args) => {
-            // If you need a specific property, check for the name
-            if (args.propertyName == nameof(settingsData.SoundVolume)) {
-                Debug.Log($"Sound volume changed to {settingsData.SoundVolume}");
-            }
-            // If you need to do something with the whole object, use the property bag
-            else {
-                var bag = PropertyBag.GetPropertyBag<MainMenuSettingsData>();
-                var data = (MainMenuSettingsData)sender;
-                // Don't use the bag collection with linq too often, it's slow, but this is a demo
-                // Normally you'd use a foreach(var prop in bag.GetProperties())
+    void SetupAutoSave()
+    {
+        settingsData.propertyChanged += (sender, args) =>
+        {
+            settingsData.Save();
 
-                var value = bag
-                    .GetProperties() // Gets all the properties
-                    // prop has Name and Get/Set value methods, check docs for more.
-                    // Here I'm just finding the property that matches the event
-                    .First(prop =>
-                        prop.Name ==
-                        args.propertyName)
-                    // You need to pass the data object, as the bag only holds the "shape" of the object, not the object itself
-                    .GetValue(ref data);
-                Debug.Log(
-                    $"{args.propertyName} prop changed to {value}");
-            }
+            Debug.Log($"Settings changed: {args.propertyName} - autosaved.");
         };
+    }
 }
